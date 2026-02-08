af927f1f0e2c43ed96a192b4c9ec1ae83b237368
= <hlong03022k6@gmail.com>
2026-02-08 17:28:53 +0700

    build SignUpScreen part 1(Group link of facebook, google to sign up into GroupSocialButtons, create Button sign up)

--- app/src/main/java/com/landt/unifoodapp/ui/Widgets.kt
+++ app/src/main/java/com/landt/unifoodapp/ui/Widgets.kt
@@ -1,6 +1,8 @@
 package com.landt.unifoodapp.ui
 
+import android.graphics.Color.alpha
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,18 +12,28 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.landt.unifoodapp.R
+import com.landt.unifoodapp.ui.theme.Orange
 
 @Composable
 fun GroupSocialButtons(
@@ -100,5 +112,63 @@ fun SocialButton(
                 ), color = Color.White
             )
         }
+    }
+}
+
+@Composable
+fun UniFoodTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource? = null,
+    shape: Shape = OutlinedTextFieldDefaults.shape,
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors().copy(
+        focusedIndicatorColor = Orange,
+        unfocusedIndicatorColor = Color.LightGray.copy(alpha = 0.4f),
+    )
+) {
+    Column {
+        OutlinedTextField(
+            value = value,
+            onValueChange,
+            modifier,
+            enabled,
+            readOnly,
+            textStyle,
+            null,
+            placeholder,
+            leadingIcon,
+            trailingIcon,
+            prefix,
+            suffix,
+            supportingText,
+            isError,
+            visualTransformation,
+            keyboardOptions,
+            keyboardActions,
+            singleLine,
+            maxLines,
+            minLines,
+            interactionSource,
+            shape,
+            colors
+        )
     }
 }
--- app/src/main/java/com/landt/unifoodapp/ui/features/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/landt/unifoodapp/ui/features/auth/signup/SignUpScreen.kt
@@ -1,21 +1,82 @@
 package com.landt.unifoodapp.ui.features.auth.signup
 
+import android.widget.Button
+import android.widget.Space
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.landt.unifoodapp.R
+import com.landt.unifoodapp.ui.GroupSocialButtons
+import com.landt.unifoodapp.ui.theme.Orange
 
 @Composable
 fun SignUpScreen() {
-    Box(modifier = Modifier.fillMaxSize()){
-        Image(painter = painterResource(id = R.drawable.ic_auth_bg), contentDescription = null,
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_auth_bg), contentDescription = null,
             modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.FillBounds)
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(modifier = Modifier.weight(1f))
+            Button(
+                onClick = {},
+                modifier = Modifier.height(48.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Orange),
+                shape = RoundedCornerShape(32.dp),
+
+                ) {
+                Text(
+                    text = stringResource(id = R.string.sign_up),
+                    modifier = Modifier.padding(horizontal = 32.dp)
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .size(16.dp)
+                    .fillMaxWidth()
+                    .background(Color.Black)
+            )
+            Text(
+                text = stringResource(id = R.string.already_have_account),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable {}
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            GroupSocialButtons(color = Color.Black, onFacebookClick = {})
+            { }
+        }
     }
 }
 
