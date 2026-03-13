67ddbbbaccfae475c19dc83ed3e0025c6e3124f8
= <hlong03022k6@gmail.com>
2026-03-13 22:07:13 +0700

    Create sign in ÆÆwith facebook

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,5 +70,7 @@ dependencies {
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
+    implementation("com.facebook.android:facebook-android-sdk:[4,5)")
 }
 
