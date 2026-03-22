e4c8222796a18cde899658e31672775f45dd112c
= <hlong03022k6@gmail.com>
2026-03-22 22:00:43 +0700

    add ApiResponse in remote package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,6 @@ dependencies {
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 
-    implementation("com.facebook.android:facebook-android-sdk:[4,5)")
+//    implementation("com.facebook.android:facebook-android-sdk:[4,5)")
 }
 
