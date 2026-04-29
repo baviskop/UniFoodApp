f809b04266bd51b80834f9cf95d34481d734666f
KezaRep <tuanta.24it@vku.udn.vn>
2026-04-30 03:04:29 +0700

    Sửa package

--- app/src/main/java/com/landt/unifoodapp/data/models/ResturauntsResponse.kt
+++ app/src/main/java/com/landt/unifoodapp/data/models/ResturauntsResponse.kt
@@ -1,6 +1,4 @@
-package com.codewithfk.foodhub.data.models
-
-import com.landt.unifoodapp.data.models.Restaurant
+package com.landt.unifoodapp.data.models
 
 data class ResturauntsResponse(
     val `data`: List<Restaurant>
